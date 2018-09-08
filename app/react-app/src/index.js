3e11bc2b52dc277e6d9643b70ccdc2b607836f2d
Shreya Chandrasekar <shreya@Shreyas-MacBook-Pro.local>
2018-09-08 18:58:08 -0400

    Restructured react-app

---
+++ app/react-app/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import * as d3 from "d3";
+
+ReactDOM.render(<App />, document.getElementById('root'));
