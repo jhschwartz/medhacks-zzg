efa1ba33024800df1228b2cbe85f2aeb38c5853d
Hycho-lah <elissa_ye@hotmail.com>
2018-09-09 06:52:02 -0400

    EEG Analysis for generating graphs

---
+++ frontend/js/ZZG_Analysis.js
@@ -0,0 +1,635 @@
+var eeg_data = [
+	{
+	  "eeg": [
+	    12.0,
+	    0.0,
+	    150.0,
+      8203.0,
+      8225.0,
+      8183.0,
+      6346.0,
+      4439.0,
+      150.0,
+      8203.0,
+      8225.0,
+      8183.0,
+      6346.0,
+      4439.0,
+      8225.0,
+      8183.0,
+      6346.0,
+	    0.0,
+	    0.0
+	  ],
+	  "sid": "af349e3e-c72b-44c9-992c-5ee1905cfdaa",
+	  "time": 1283.58928052493
+	},
+	{
+	  "eeg": [
+	    13.0,
+	    0.0,
+	    150.0,
+      8203.0,
+      8225.0,
+      8183.0,
+      6346.0,
+      4439.0,
+      150.0,
+      8203.0,
+      8225.0,
+      8183.0,
+      6346.0,
+      8225.0,
+      8183.0,
+      6346.0,
+      4439.0,
+	    0.0,
+	    0.0
+	  ],
+	  "sid": "af349e3e-c72b-44c9-992c-5ee1905cfdaa",
+	  "time": 1383.58928052493
+	},
+	{
+	  "eeg": [
+	    14.0,
+	    0.0,
+	    150.0,
+      8203.0,
+	    8225.0,
+	    8183.0,
+	    4439.0,
+      150.0,
+	    8203.0,
+	    8225.0,
+	    8183.0,
+	    6346.0,
+	    4439.0,
+      8225.0,
+	    8183.0,
+	    6346.0,
+	    4439.0,
+	    0.0,
+	    0.0
+	  ],
+	  "sid": "af349e3e-c72b-44c9-992c-5ee1905cfdaa",
+	  "time": 1483.58928052493
+	},
+	{
+	  "eeg": [
+	    15.0,
+	    0.0,
+	    150.0,
+	    8203.0,
+	    8225.0,
+	    8183.0,
+	    6346.0,
+      150.0,
+	    8203.0,
+	    8225.0,
+	    8183.0,
+	    6346.0,
+	    4439.0,
+      8225.0,
+	    8183.0,
+	    6346.0,
+	    4439.0,
+	    0.0,
+	    0.0
+	  ],
+	  "sid": "af349e3e-c72b-44c9-992c-5ee1905cfdaa",
+	  "time": 1583.58928052493
+	}
+]
+
+var power_data = [
+	{
+    "pow":[
+      64.0,
+      46.0,
+      66.0,
+      65.0,
+      71.0,
+      42.0,
+      57.0,
+      37.0,
+      369.0,
+      76.0,
+      22.0,
+      40.0,
+      17.0,
+      50.0,
+      41.0,
+      14.0,
+      10.0,
+      31.0,
+      26.0,
+      30.0,
+      20.0,
+      36.0,
+      66.0,
+      65.0,
+      71.0,
+      42.0,
+      57.0,
+      37.0,
+      369.0,
+      76.0,
+      64.0,
+      46.0,
+      66.0,
+      65.0,
+      71.0,
+      42.0,
+      57.0,
+      37.0,
+      369.0,
+      76.0,
+      22.0,
+      40.0,
+      17.0,
+      50.0,
+      41.0,
+      14.0,
+      10.0,
+      31.0,
+      26.0,
+      30.0,
+      20.0,
+      36.0,
+      66.0,
+      65.0,
+      71.0,
+      42.0,
+      57.0,
+      37.0,
+      369.0,
+      76.0,
+      64.0,
+      46.0,
+      66.0,
+      65.0,
+      71.0,
+      42.0,
+      57.0,
+      37.0,
+      369.0,
+      76.0
+    ],
+    "sid":"d8834c5f-ae19-4a3c-8ce0-a861c5e05daf"
+  },
+  {
+    "pow":[
+      64.0,
+      46.0,
+      66.0,
+      65.0,
+      71.0,
+      42.0,
+      57.0,
+      37.0,
+      369.0,
+      76.0,
+      22.0,
+      40.0,
+      17.0,
+      50.0,
+      41.0,
+      14.0,
+      10.0,
+      31.0,
+      26.0,
+      30.0,
+      20.0,
+      36.0,
+      66.0,
+      65.0,
+      71.0,
+      42.0,
+      57.0,
+      37.0,
+      369.0,
+      76.0,
+      64.0,
+      46.0,
+      66.0,
+      65.0,
+      71.0,
+      42.0,
+      57.0,
+      37.0,
+      369.0,
+      76.0,
+      22.0,
+      40.0,
+      17.0,
+      50.0,
+      41.0,
+      14.0,
+      10.0,
+      31.0,
+      26.0,
+      30.0,
+      20.0,
+      36.0,
+      66.0,
+      65.0,
+      71.0,
+      42.0,
+      57.0,
+      37.0,
+      369.0,
+      76.0,
+      64.0,
+      46.0,
+      66.0,
+      65.0,
+      71.0,
+      42.0,
+      57.0,
+      37.0,
+      369.0,
+      76.0
+    ],
+    "sid":"d8834c5f-ae19-4a3c-8ce0-a861c5e05daf"
+  },
+  {
+    "pow":[
+      64.0,
+      46.0,
+      66.0,
+      65.0,
+      71.0,
+      42.0,
+      57.0,
+      37.0,
+      369.0,
+      76.0,
+      22.0,
+      40.0,
+      17.0,
+      50.0,
+      41.0,
+      14.0,
+      10.0,
+      31.0,
+      26.0,
+      30.0,
+      20.0,
+      36.0,
+      66.0,
+      65.0,
+      71.0,
+      42.0,
+      57.0,
+      37.0,
+      369.0,
+      76.0,
+      64.0,
+      46.0,
+      66.0,
+      65.0,
+      71.0,
+      42.0,
+      57.0,
+      37.0,
+      369.0,
+      76.0,
+      22.0,
+      40.0,
+      17.0,
+      50.0,
+      41.0,
+      14.0,
+      10.0,
+      31.0,
+      26.0,
+      30.0,
+      20.0,
+      36.0,
+      66.0,
+      65.0,
+      71.0,
+      42.0,
+      57.0,
+      37.0,
+      369.0,
+      76.0,
+      64.0,
+      46.0,
+      66.0,
+      65.0,
+      71.0,
+      42.0,
+      57.0,
+      37.0,
+      369.0,
+      76.0
+    ],
+    "sid":"d8834c5f-ae19-4a3c-8ce0-a861c5e05daf"
+  },
+  {
+    "pow":[
+      64.0,
+      46.0,
+      66.0,
+      65.0,
+      71.0,
+      42.0,
+      57.0,
+      37.0,
+      369.0,
+      76.0,
+      22.0,
+      40.0,
+      17.0,
+      50.0,
+      41.0,
+      14.0,
+      10.0,
+      31.0,
+      26.0,
+      30.0,
+      20.0,
+      36.0,
+      66.0,
+      65.0,
+      71.0,
+      42.0,
+      57.0,
+      37.0,
+      369.0,
+      76.0,
+      64.0,
+      46.0,
+      66.0,
+      65.0,
+      71.0,
+      42.0,
+      57.0,
+      37.0,
+      369.0,
+      76.0,
+      22.0,
+      40.0,
+      17.0,
+      50.0,
+      41.0,
+      14.0,
+      10.0,
+      31.0,
+      26.0,
+      30.0,
+      20.0,
+      36.0,
+      66.0,
+      65.0,
+      71.0,
+      42.0,
+      57.0,
+      37.0,
+      369.0,
+      76.0,
+      64.0,
+      46.0,
+      66.0,
+      65.0,
+      71.0,
+      42.0,
+      57.0,
+      37.0,
+      369.0,
+      76.0
+    ],
+    "sid":"d8834c5f-ae19-4a3c-8ce0-a861c5e05daf"
+  }
+]
+
+//Function for transposing 2D arrays.
+function transpose(array) {
+    return array.reduce((prev, next) => next.map((item, i) =>
+        (prev[i] || []).concat(next[i])
+    ), []);
+}
+
+//Get EEG Data to be analyzed. Return an array of arrays (signal values for channel x time).
+function get_eegdata(eeg_data){
+  var eeg_data_processed = [];
+  for (i=0;i<eeg_data.length;i++){
+    eeg_dic = eeg_data[i];
+    eeg_channels = eeg_dic["eeg"]
+    eeg_channels = eeg_channels.slice(3,eeg_channels.length-2);
+    eeg_data_processed[i] = eeg_channels;
+  }
+  eeg_data_processed = transpose(eeg_data_processed);
+  return eeg_data_processed
+}
+
+//Get Power Data to be analyzed. Return an array of arrays (band power values for each channel x time).
+function get_powerdata(power_data){
+  var power_data_processed = [];
+  for (i=0;i<power_data.length;i++){
+    power_dic = power_data[i];
+    power_channels = power_dic["pow"]
+    power_data_processed[i] = power_channels;
+  }
+  power_data_processed = transpose(power_data_processed);
+  return power_data_processed
+}
+
+//Get processed data
+var eeg_data_processed = get_eegdata(eeg_data);
+var power_data_processed = get_powerdata(power_data);
+
+//Get time interval windows for a signal channel
+function get_intervals(data_row,window_size){
+  var intervals = [];
+  var interval = [];
+  for (i=0; i<data_row.length;i++){
+    //if ((i+1)<window_size){
+      //for (j=0;j<(window_size-i);j++){
+        //interval.push(data_row[0]);
+        //interval.concat(data_row.slice(0,i));
+      //}
+    //} else {
+    if ((i+1)>window_size){
+      interval = data_row.slice(i-window_size,i);
+      intervals.push(interval);
+    }
+  }
+  return intervals
+}
+
+//SLEEP QUALITY
+// x is the time window of 350 datapoints(IED channels from EEG data).
+// Returns one datapoint representing entropy.
+
+function y=entropy(x){
+  var tot = 0.0;
+  var ent = 0.0;
+  for (i=0;i < x.length; i++){
+    tot = tot + x[i]ˆ2;
+  }
+  for (i=0;i < x.length; i++){
+    quo = x[i]ˆ2 / tot;
+    ent = ent + (quo * log10(quo));
+  }
+  var y = -ent;
+  return y
+}
+
+var entropy_matrix = [];
+//Calculate entropy for each data point. i for each row of channel.
+for (i=0;i < eeg_data_processed.length; i++){
+  windows_values = get_intervals(eeg_data_processed[i],350);
+  for (w=0;i<windows_values.length;w++){
+    entropy_matrix[i][w] = entropy(windows_values[w])
+  }
+}
+
+var column_avg = col => {
+	var sum = 0.0
+	col.map(e => sum += e)
+	return sum / col.length
+}
+
+//Use this to create chart
+entropy_data = entropy_matrix.map(e => column_avg(e));
+
+//Sums arrays
+function sumArray(a, b) {
+      var c = [];
+      for (var i = 0; i < Math.max(a.length, b.length); i++) {
+        c.push((a[i] || 0) + (b[i] || 0));
+      }
+      return c;
+}
+
+//Divide arrays
+function divideArray(array, divisor) {
+    var i = array.length, a, k;
+    while (i) { // loop over each item in array
+        a = array[--i];
+        for (k in a) { // loop over each key in object
+            if (a.hasOwnProperty(k)) { // ignore inherited keys
+                a[k] = a[k] / divisor; // calculate
+            }
+        }
+    }
+    return array;
+}
+
+//WAVES
+var theta_waves = sumArray(power_data_processed[0],power_data_processed[5])
+theta_waves = sumArray(theta_waves,power_data_processed[10]);
+theta_waves = sumArray(theta_waves,power_data_processed[15]);
+theta_waves = sumArray(theta_waves,power_data_processed[20]);
+theta_waves = sumArray(theta_waves,power_data_processed[25]);
+theta_waves = sumArray(theta_waves,power_data_processed[30]);
+theta_waves = sumArray(theta_waves,power_data_processed[35]);
+theta_waves = sumArray(theta_waves,power_data_processed[40]);
+theta_waves = sumArray(theta_waves,power_data_processed[45]);
+theta_waves = sumArray(theta_waves,power_data_processed[50]);
+theta_waves = sumArray(theta_waves,power_data_processed[55]);
+theta_waves = sumArray(theta_waves,power_data_processed[60]);
+theta_waves = sumArray(theta_waves,power_data_processed[65]);
+theta_waves = divideArray(theta_waves,14)
+
+var alpha_waves = sumArray(power_data_processed[1],power_data_processed[6])
+alpha_waves = sumArray(alpha_waves,power_data_processed[11]);
+alpha_waves = sumArray(alpha_waves,power_data_processed[16]);
+alpha_waves = sumArray(alpha_waves,power_data_processed[21]);
+alpha_waves = sumArray(alpha_waves,power_data_processed[26]);
+alpha_waves = sumArray(alpha_waves,power_data_processed[31]);
+alpha_waves = sumArray(alpha_waves,power_data_processed[36]);
+alpha_waves = sumArray(alpha_waves,power_data_processed[41]);
+alpha_waves = sumArray(alpha_waves,power_data_processed[46]);
+alpha_waves = sumArray(alpha_waves,power_data_processed[51]);
+alpha_waves = sumArray(alpha_waves,power_data_processed[56]);
+alpha_waves = sumArray(alpha_waves,power_data_processed[61]);
+alpha_waves = sumArray(alpha_waves,power_data_processed[66]);
+alpha_waves = divideArray(alpha_waves,14)
+
+var beta_waves = sumArray(power_data_processed[2],power_data_processed[7])
+beta_waves = sumArray(beta_waves,power_data_processed[12]);
+beta_waves = sumArray(beta_waves,power_data_processed[17]);
+beta_waves = sumArray(beta_waves,power_data_processed[22]);
+beta_waves = sumArray(beta_waves,power_data_processed[27]);
+beta_waves = sumArray(beta_waves,power_data_processed[32]);
+beta_waves = sumArray(beta_waves,power_data_processed[37]);
+beta_waves = sumArray(beta_waves,power_data_processed[42]);
+beta_waves = sumArray(beta_waves,power_data_processed[47]);
+beta_waves = sumArray(beta_waves,power_data_processed[52]);
+beta_waves = sumArray(beta_waves,power_data_processed[57]);
+beta_waves = sumArray(beta_waves,power_data_processed[62]);
+beta_waves = sumArray(beta_waves,power_data_processed[67]);
+beta_waves = divideArray(beta_waves,14)
+
+var gamma_waves = sumArray(power_data_processed[4],power_data_processed[9])
+gamma_waves = sumArray(gamma_waves,power_data_processed[14]);
+gamma_waves = sumArray(gamma_waves,power_data_processed[19]);
+gamma_waves = sumArray(gamma_waves,power_data_processed[24]);
+gamma_waves = sumArray(gamma_waves,power_data_processed[29]);
+gamma_waves = sumArray(gamma_waves,power_data_processed[34]);
+gamma_waves = sumArray(gamma_waves,power_data_processed[39]);
+gamma_waves = sumArray(gamma_waves,power_data_processed[44]);
+gamma_waves = sumArray(gamma_waves,power_data_processed[49]);
+gamma_waves = sumArray(gamma_waves,power_data_processed[54]);
+gamma_waves = sumArray(gamma_waves,power_data_processed[59]);
+gamma_waves = sumArray(gamma_waves,power_data_processed[64]);
+gamma_waves = sumArray(gamma_waves,power_data_processed[69]);
+gamma_waves = divideArray(gamma_waves,14)
+
+//When displaying the graph, multiply entropy by -1 and adjust the scale appropriately for better visuals.
+
+//SLEEP STAGES
+//Start session with Stage 1. End Stage 1 with start of Stage 2.
+// Stage 1 has mixed-frequency EEG tracing with a considerable representation of theta-wave activity (4–7 Hz) and some alpha-activity.
+//Stage 2 start with K-complexes and sleep spindles less than 3 minutes apart.
+//Stage 2 is characterized by typical intermittent short sequences of waves of 11–15 Hz (“sleep spindles”) and decreased alpha-activity.
+//Stage 3 is when slow waves occupy more than 20 percent of the 30-second window of an EEG tracing.
+//Stage 3 has high delta
+//Stage 4 has higher delta
+//Stage 5 has dramatic decrease in delta power
+//Stage 5 ends with increase in theta
+//Max delta power during stage 4  normalized as 100
+
+//initializations
+var stage = 1;
+//time intervals for different stages of sleep
+stage_1_duration = [0]
+stage_2_duration = []
+stage_3_duration = []
+stage_4_duration = []
+stage_4_duration = []
+
+delta_power =
+
+//Loop for Session
+while (stage<=5)
+	for 
+  if (stage == 1 && delta_power > 30){
+    stage = 2;
+    stage_1_duration.push(time);
+    stage_2_duration.push(time+1);
+    }
+  if (stage == 2 && delta_power > 40){
+    stage = 3;
+    stage_2_duration.push(time);
+    stage_3_duration.push(time+1);
+  }
+  if (stage == 3 && delta_power > 80){
+    stage = 4;
+    stage_3_duration.push(time);
+    stage_4_duration.push(time+1);
+  }
+  if (stage == 4 && delta_power < 50){
+    stage = 5;
+    stage_4_duration.push(time);
+    stage_5_duration.push(time+1);
+  }
+  if (stage ==5 && theta > 30 && delta_power < 30){
+    stage = 0;
+    stage_5_duration.push(time);
+	}
+
+}
